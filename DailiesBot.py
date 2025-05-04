f4eaa76c5651b16e00f850d9ed4a65c2e5d33cbf
eyeprism <bm2alt2@gmail.com>
2025-05-04 00:29:22 -0500

    attemped enkephalin fix

--- DailiesBot.py
+++ DailiesBot.py
@@ -61,19 +61,29 @@ def findState():
         return 4
     if locateOnScreenBool("Images/Battle_Winrate.png", confidence = 0.8, grayscale = True, region = (800,750,1120,200)):#1515 765, 1815 900
         return 5
-    if locateOnScreenBool("Images/ConfirmEnd.png", confidence = 0.8, grayscale = True, region = (1515,765,300,135)):#you win
+    if locateOnScreenBool("Images/End_Victory.png", confidence = 0.8, grayscale = True, region = (1375,150,500,500)):#you win
         return 6
     if locateOnScreenBool("Images/Battle_Skill.png", confidence = 0.8, grayscale = True, region = (1172,371,1162,58)):
         return 7
-    if locateOnScreenBool("Images/ChargeEnkephalin.png", confidence = 0.8, grayscale = True, region = (740, 146,1172-740,212-146)):
+    if locateOnScreenBool("Images/Enkephalin_CanAssemble.png", confidence = 0.8, grayscale = True, region = (650, 710, 600, 60)):
         return 8
+    if locateOnScreenBool("Images/Luxcavation_DailyBonuses.png", confidence = 0.8, grayscale = True, region = (720, 40, 180, 40)):
+        return 9
+    if locateOnScreenBool("Images/End_Defeat.png", confidence = 0.8, grayscale = True, region = (1475,192,300,150)):
+        return 10
+    if locateOnScreenBool("Images/Enkephalin_DailyCount.png", confidence = 0.8, grayscale = True, region = (990, 410, 250, 45)):
+        return 11
+    if locateOnScreenBool("Images/Enkephalin_Insufficient.png", confidence = 0.8, grayscale = True, region = (800, 710, 300, 60)):
+        return 12
+
+    #720, 40
     return -1
     
 
 def doEXPLux():
     done = False
     while not done:
-        time.sleep(0.1)
+        time.sleep(0.5)
         state = findState()
         print(state)
         match state:
@@ -90,11 +100,12 @@ def doEXPLux():
                 pyautogui.click(1662, 711)
             case 4:
                 pyautogui.click(1715, 720)
-                time.sleep(0.5)
+                time.sleep(1)
                 pyautogui.click(1145, 740)
-                time.sleep(0.5)
+                time.sleep(1)
                 for i in range(6):
                     pyautogui.click(sinnerCoordinates[EXP_team[i].lower()])
+                    time.sleep(0.2)
                 time.sleep(.5)
                 pyautogui.click(1720,880)
                 time.sleep(5)
@@ -109,10 +120,15 @@ def doEXPLux():
                 pyautogui.click(1660,840)
                 done = True
             case 7:
+                time.sleep(0.5)
                 pyautogui.click(100, 100)
+            case 10:
+                time.sleep(0.5)
+                pyautogui.click(1660,840)
+            
 def doThreadLux(skips):
     while skips > 0:
-        time.sleep(0.1)
+        time.sleep(0.5)
         state = findState()
         print(state)
         match state:
@@ -127,12 +143,6 @@ def doThreadLux(skips):
                 pyautogui.click(666,250)
             case 3:
                 pyautogui.click(200, 500)
-                time.sleep(0.5)
-                pyautogui.click(555, 770)
-                time.sleep(0.5)
-                pyautogui.click(935, 720)
-                time.sleep(0.5)
-                pyautogui.click(1140, 730)
             case 5:#OMG P-ENTER!!!
                 pyautogui.click(width/2,height/6)
                 time.sleep(0.05)
@@ -145,11 +155,18 @@ def doThreadLux(skips):
                 skips -= 1
             case 7:
                 pyautogui.click(100, 100)
+            case 9:
+                pyautogui.click(555, 770)
+                time.sleep(0.5)
+                pyautogui.click(935, 720)
+                time.sleep(0.5)
+                pyautogui.click(1140, 730)
+                time.sleep(1)
 
-def convertModules():
-    done = False
-    while not done:
-        time.sleep(0.1)
+def convertModules(refills):
+    assembled = False
+    while refills > 0 and assembled == False:
+        time.sleep(0.5)
         state = findState()
         print(state)
         match state:
@@ -157,17 +174,35 @@ def convertModules():
                 pyautogui.click(x = width/2, y = height/2)
             case 1:
                 pyautogui.click(570, 1000)
+                time.sleep(2)
+                pyautogui.click(950, 340)#use lunacy tab
+                time.sleep(0.5)
             case 8:#convert modules
-                pyautogui.click(950, 340)
+
+                #pyautogui.click(950, 340)#use lunacy tab
+                #time.sleep(0.5)
+                #pyautogui.click(1132, 800)#confirm
+                #time.sleep(0.5)
+                #pyautogui.click(750, 340)#modules tab
+                #time.sleep(0.5)
+                pyautogui.click(1200, 500)#right arrow
                 time.sleep(0.5)
-                pyautogui.click(1132, 800)
+                pyautogui.click(1132, 800)#confirm
                 time.sleep(0.5)
-                pyautogui.click(750, 340)
-                time.sleep(0.5)
-                pyautogui.click(1200, 500)
-                time.sleep(0.5)
-                pyautogui.click(1132, 800)
-                done = True
+                #pyautogui.click(770, 800)#cancel
+                #refills -= 1
+            case 11:
+                if refills > 0:
+                    time.sleep(2)
+                    pyautogui.click(1132, 800)#confirm
+                    refills -= 1
+                else:
+                    time.sleep(2)
+                    pyautogui.click(750, 340)#modules tab
+                    time.sleep(0.5)
+            case 12:
+                pyautogui.click(770, 800)#cancel
+                assembled = True
                 
 
 
@@ -186,7 +221,13 @@ def mainFunc():
         except:
             print("num must be integer")
             quit()
-    convertModules()
+    refills = pyautogui.prompt(text = "how many lunacy enkephalin refills?", title = "StartMenu", default = 1)
+    try:
+        refills = int(refills)
+    except:
+        print("Num refills must be integer")
+        quit()
+    convertModules(refills)
     if choice == 1 or choice == 3:
         doEXPLux()
     if choice == 2 or choice == 3:
